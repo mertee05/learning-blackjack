51bfed96bb41d827341cd0d3bf356b66515c73dd
mertee05 <131835458+mertee05@users.noreply.github.com>
2023-06-10 15:58:45 -0700

    random number functionality
    Added the ability to generate random card numbers between 1 and 13

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let firstCard = 10;
-let secondCard = 4;
+let firstCard = getRandomCard();
+let secondCard = getRandomCard();
 let cards = [firstCard, secondCard];
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
@@ -9,12 +9,20 @@ let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
 
+function getRandomCard() {
+  return Math.floor(Math.random() * 13) + 1; // 1-13
+}
+
 function startGame() {
   renderGame();
 }
 
 function renderGame() {
-  cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1];
+  cardsEl.textContent = "Cards: ";
+  for (let i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + " ";
+  }
+
   sumEl.textContent = "Sum: " + sum;
   if (sum <= 20) {
     message = "Do you want to draw a new card?";
@@ -29,9 +37,8 @@ function renderGame() {
 }
 
 function newCard() {
-  let card = 6;
+  let card = getRandomCard();
   sum += card;
-  // Push the card to the cards array
   cards.push(card);
   renderGame();
 }
